0852dcee9dc846d0a44abc1cf68e3de05fec4d20
TaegeunYou <66227661+TaegeunYou@users.noreply.github.com>
2021-07-13 05:59:09 +0900

    Initial commit

---
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = true)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lasModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Member() {
+    }
+}

4bc6bf12ed55975c46cced1b1d29bd6b2a7d9b0a
TaegeunYou <66227661+TaegeunYou@users.noreply.github.com>
2021-07-14 05:12:49 +0900

    객체 설계를 테이블 설계에 맞춘 방식

---
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //db당 하나만
+
+        EntityManager em = emf.createEntityManager();   //고객의 요청이 올때마다
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
